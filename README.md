dc4070825f86a04d89f7a42eebcc905e0aca3698
Andrea Roveroni <roveroniandrea@gmail.com>
2020-08-25 10:26:34 +0200

    readme game working

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-## Hello there 👋
-![Coding image](https://github.com/roveroniandrea/roveroniandrea/blob/master/images/coding-resize.gif)
+<!--## Hello there 👋
+![Coding image](https://github.com/roveroniandrea/roveroniandrea/blob/master/images/coding-resize.gif)-->
+# Hangman game
+You can play the hangman game on this readme!
+<!--GAME-->
+## You lost!
+[New game](https:&#x2F;&#x2F;github.com&#x2F;roveroniandrea&#x2F;roveroniandrea&#x2F;issues&#x2F;new?title&#x3D;hangman%7Cnew-game&amp;body&#x3D;Just+push+%27Submit+new+issue%27+without+editing+the+title.+The+README+will+be+updated+after+approximately+30+seconds.)
 
-<!--
-**roveroniandrea/roveroniandrea** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+## The word to guess was:
+```
+I n v o l u c r u m
+```
+## Tries remaining: 0
 
-Here are some ideas to get you started:
-
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+| Letters called |
+| -----------    | ----------- |
+|       a        |
+<!--DATAeyJ3b3JkIjoiaW52b2x1Y3J1bSIsImd1ZXNzZWQiOlsxLDEsMSwxLDEsMSwxLDEsMSwxXSwidHJpZXNSZW1haW5pbmciOjAsImxldHRlcnNDYWxsZWQiOlsiYSJdfQ==DATA-->
---
+++ index.js
@@ -0,0 +1,114 @@
+const randomWord = require('random-word');
+const fs = require('fs');
+const { Base64 } = require('js-base64');
+const Mustache = require('mustache');
+
+const readme = 'README.md';
+const gameMustache = 'game.mustache';
+const dataFormat = ['<!--DATA', 'DATA-->'];
+const newGameSyntax = 'new-game';
+
+if (!process.argv[2]) {
+  // Scelgo una parola da indovinare
+  let word = '';
+  do {
+    word = randomWord();
+  } while (word.length < 7);
+
+  const triesRemaining = word.length - 3;
+  const guessed = word.split('').map((_, index) => index == 0 || index == word.length - 1);
+
+  renderToReadme({
+    secretWord: formatWord(word, guessed),
+    data: `${dataFormat[0]}${encodeSolution(word, guessed, triesRemaining, [])}${dataFormat[1]}`,
+    triesRemaining,
+    lettersCalled: [],
+    lost: false,
+    keyboard: renderKeyboard(),
+    newGame: newGameUrl(),
+  });
+} else {
+  const letter = process.argv[2].toLowerCase();
+  const file = fs.readFileSync(readme).toString();
+  const decodedData = decodeSolution(file.substring(file.indexOf(dataFormat[0]) + dataFormat[0].length, file.indexOf(dataFormat[1])));
+
+  if (!decodedData.lettersCalled.includes(letter)) {
+    decodedData.lettersCalled.unshift(letter);
+    const isValid = decodedData.word.includes(letter);
+    if (isValid) {
+      decodedData.guessed = decodedData.word.split('').map((ch, index) => {
+        return decodedData.guessed[index] || ch === letter;
+      });
+    } else {
+      decodedData.triesRemaining = parseInt(decodedData.triesRemaining) - 1;
+    }
+  } else {
+    decodedData.triesRemaining = parseInt(decodedData.triesRemaining) - 1;
+  }
+  if (decodedData.triesRemaining <= 0) {
+    decodedData.guessed = decodedData.guessed.fill(1);
+  }
+
+  renderToReadme({
+    secretWord: formatWord(decodedData.word, decodedData.guessed),
+    data: `${dataFormat[0]}${encodeSolution(decodedData.word, decodedData.guessed, decodedData.triesRemaining, decodedData.lettersCalled)}${
+      dataFormat[1]
+    }`,
+    triesRemaining: decodedData.triesRemaining,
+    lettersCalled: decodedData.lettersCalled,
+    lost: decodedData.triesRemaining <= 0,
+    keyboard: renderKeyboard(),
+    newGame: newGameUrl(),
+  });
+}
+
+function formatWord(word, guessed) {
+  return word
+    .split('')
+    .map((character, index) => {
+      if (guessed[index]) {
+        return index == 0 ? character.toUpperCase() : character;
+      } else {
+        return '_';
+      }
+    })
+    .join(' ');
+}
+
+function encodeSolution(word, guessed, triesRemaining, lettersCalled) {
+  const obj = {
+    word,
+    guessed,
+    triesRemaining,
+    lettersCalled,
+  };
+
+  const encoded = Base64.encode(JSON.stringify(obj));
+  return encoded;
+}
+
+function decodeSolution(encoded) {
+  return JSON.parse(Base64.decode(encoded));
+}
+
+function renderToReadme(view) {
+  const mustacheFile = fs.readFileSync(gameMustache).toString();
+  const newFile = Mustache.render(mustacheFile, view);
+  fs.writeFileSync(readme, newFile);
+}
+
+function renderKeyboard() {
+  const letters = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
+  const result = [];
+  letters.forEach((row, rowIndex) => {
+    result[rowIndex] = row.split('').map((letter) => ({
+      letter,
+      url: `https://github.com/roveroniandrea/roveroniandrea/issues/new?title=hangman%7C${letter}&body=Just+push+%27Submit+new+issue%27+without+editing+the+title.+The+README+will+be+updated+after+approximately+30+seconds.`,
+    }));
+  });
+  return result;
+}
+
+function newGameUrl(){
+  return `https://github.com/roveroniandrea/roveroniandrea/issues/new?title=hangman%7C${newGameSyntax}&body=Just+push+%27Submit+new+issue%27+without+editing+the+title.+The+README+will+be+updated+after+approximately+30+seconds.`
+}
